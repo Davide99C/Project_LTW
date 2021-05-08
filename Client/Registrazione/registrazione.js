dcd7c765b4dbe9c64240d33e6f406d818f8e341b
Davide Chiarabini <davidechiarabini@hotmail.it>
2021-05-08 13:13:58 +0200

    server_registrazione

--- Client/Registrazione/registrazione.js
+++ Client/Registrazione/registrazione.js
@@ -1,81 +1,3 @@
-$(document).ready(function() {
-
-  $('#login').click(function(){
-    var userValue = $('#email').val();
-    var pwdValue = $('#password1').val();
-    var pwdValue2 = $('#password2').val();
-
-    if(pwdValue != pwdValue2){
-        passwordWrong(pwdValue, pwdValue2);
-    }else{
-        $.ajax({
-            type: "POST",
-            url: 'http://localhost/Progetto_LTW/Project_LTW/Client/php/registrazione.php',
-            data: ({ email : userValue, password: pwdValue }),
-            dataType: "html",
-            success: function(data) {
-                checkLogin( data );
-                return data;
-            },
-            error: function() {
-                console.log('Error occured');
-            }
-        });//ajax
-    }
-  });//click
-
-});//jq
-
-function checkLogin(data){
-  if(data == "ok"){
-     var login_box = document.getElementsByClassName('login_box');
-     login_box[0].innerHTML = "<h1 style='color:white;'> Account Creato </h1>";
-      //window.alert("Account creato");
-     setInterval(() => {window.location = "login.html";}, 2000);
-  }else{
-    emailWrong();
-  }
-};
-
-function emailWrong(){
-    var input = document.getElementsByClassName('sign_error');
-    input[0].innerText = "Email Already Exist";
-    setTimeout(() => {input[0].innerText = "";},5000);
-    console.log("fatto");
-
-}
-
-function passwordWrong(){
-    var input = document.getElementsByClassName('sign_error');
-    input[1].innerText = "Password Doesn't match";
-    input[2].innerText = "Password Doesn't match";
-    setTimeout(() => {
-        input[1].innerText = "";
-        input[2].innerText = "";},5000);
-    console.log("fatto");
-}
-
- function validateForm() {
-    if (document.pass1.value != document.pass2.value) {
-        alert("Errore: Nel campo di conferma password la password non corrisponde");
-        return false;
-    }
-  }
-
-//////////////////////////////////////////////////////////////////////////////////
-
-function openNav() {
-    document.getElementById("comparsa").style.width = "230px";
-    document.getElementById("main").style.marginLeft = "230px";
-    
-  }
-  
-  function closeNav() {
-    document.getElementById("comparsa").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    
-  }
-
 /*CARICAMENTO
 window.addEventListener(
   "scroll",
@@ -97,7 +19,42 @@ window.addEventListener(
     window.attachEvent("onload", nascondi_loading_screen);
   }
 })();
-function nascondi_loading_screen()
-{
+function nascondi_loading_screen() {
   document.getElementById("loading_screen").style.display = 'none';
 }
+
+///////////////////////////////////////////////////////////////////////////////
+
+ function validateForm() {
+    //var emailValue = $('#email').val();
+    var pwdValue = document.getElementById("password1").value;
+    var pwdValue2 = document.getElementById("password2").value;
+    if (pwdValue != pwdValue2) {
+        passwordWrong();
+        return false;
+    }
+  }
+  function passwordWrong(){
+    var input = document.getElementsByClassName('sign_error');
+    input[1].innerHTML = "<h5 style='color:red'>Le password non corrispondono</h5>";
+    input[2].innerHTML = "<h5 style='color:red'>Le password non corrispondono</h5>";
+    setTimeout(() => {
+        input[1].innerHTML = "";
+        input[2].innerHTML = "";},5000);
+    console.log("fatto");
+}
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+function openNav() {
+    document.getElementById("comparsa").style.width = "230px";
+    document.getElementById("main").style.marginLeft = "230px";
+    
+  }
+  
+  function closeNav() {
+    document.getElementById("comparsa").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    
+  }
--- Client/php/registrazione.php
+++ Client/php/registrazione.php
@@ -3,8 +3,11 @@
 
     $mysqli = dbConnection();
 
+    $nome = $_POST['nome'];
+    $cognome = $_POST['cognome'];
+    $data = $_POST['data'];
     $email = $_POST['email'];
-    $pass = $_POST['password'];
+    $pass = $_POST['password1'];
 
 
    $result = $mysqli -> query("SELECT * FROM Utenti where email = '$email'");
@@ -12,10 +15,18 @@
    // $row = mysqli_fetch_array($result);
 
    if($count == 0){
-       $result = $mysqli->query("INSERT INTO Utenti (email, password) VALUES ('$email','$pass')");
-       echo "ok";
+       $result = $mysqli->query("INSERT INTO Utenti (nome, cognome, data_di_nascita, email, password) VALUES ('$nome','$cognome','$data','$email','$pass')");
+       if (!$result) {
+        echo "Query failed";
+        exit();
+    }
+       $ok = "<h1 style='color:red'>Account creato correttamente</h1><h2>Attendi un istante e verrai reinderizzato alla pagina di login</h2>";
+       echo "$ok";
+       header("refresh:2; url = ../Login/login.html " );
    }else{
-       $error = "Account already Exist!";
-       die($error) ;
+       $error = "<h1 style='color:red'>L'account gia esiste!</h1><h2>Attendi un istante e verrai reinderizzato alla pagina di registrazione</h2>";
+       //die($error) ;
+       echo"$error";
+       header("refresh:2; url = ../Registrazione/registrazione.html " );
    }
 ?>
