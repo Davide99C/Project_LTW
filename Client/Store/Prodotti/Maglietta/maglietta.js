edfca87994f60eb0e0e72bbcc2f355c7b399c98e
elisadebellis <77112869+elisadebellis@users.noreply.github.com>
2021-05-10 12:01:56 +0200

    Add files via upload

--- Client/Store/Prodotti/Maglietta/maglietta.js
+++ Client/Store/Prodotti/Maglietta/maglietta.js
@@ -1,19 +1,21 @@
+
+
 Vue.component('maglietta', {
     template : '<div class="product"><div class="product-image "><img v-bind:src="image"/></div><div class="product-info"><h1>Prodotto in vendita:{{product}}</h1>Descrizione:{{description}}<p v-if="disp>10 && onSale">Disponibile</p><p v-else-if="disp>0 && onSale">Ultime scorte!</p><p v-else>Non disponibile</p><ul><li v-for="x in details">{{x.text}}</li></ul><div v-for="(x,index) in variants" :key="x.id" class="color=box"v-bind:style="{backgroundColor: x.htmlColor}"><p v-on:click="updateProduct(index)">{{x.color}}</p></div><button v-on:click="addToCart()"v-bind:disabled="!onSale || disp==0"v-bind:class="{disabledButton: !onSale || disp==0}">Aggiungi al carrello</button></div></div>'
     ,
     data: function(){
         return {
             product : 'Maglietta' ,
-            description: "Maglietta WildFishing" ,
+            description: 'Maglietta WildFishing' ,
             selectedVariant : 0,
             details : [
-                { text: "95% Cotone - 5% Elastan" } ,
-                { text: " Lavabile e riutilizzabile " } ,
-                { text: "Utilizzo ad uso civile, non medicale" }
+                { text: '95% Cotone - 5% Elastan' } ,
+                { text: ' Lavabile e riutilizzabile ' } ,
+                { text: 'Utilizzo ad uso civile, non medicale' }
                 ],
             variants : [
                 { id:2241 , color: "nera" , disp:9 , onSale : true ,
-                image:"../../../img/maglietta_front" , htmlColor: "white" } ,
+                image:"../../../../img/maglietta_front" , htmlColor: "white" } ,
                 { id:2242, color:"nera", disp:11, onSale:false ,
                 image:"../../../img/maglietta_back",htmlColor:"black" } ,
                 ]
@@ -41,10 +43,12 @@ Vue.component('maglietta', {
         }
     }
 });
+
 var app = new Vue ({
     el:'#app' ,
     data : {
-        cart : 0
+        cart : 0,
+        
     } ,
     methods : {
         updateCart:function() {
