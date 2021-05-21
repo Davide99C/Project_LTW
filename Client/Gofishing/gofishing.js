6127488cef71dd40487270ad42ece4a46972d563
Davide Chiarabini <davidechiarabini@hotmail.it>
2021-05-21 18:32:43 +0200

    privacy_policy

--- Client/Gofishing/gofishing.js
+++ Client/Gofishing/gofishing.js
@@ -83,12 +83,12 @@ function closeMap(){
 //MOSTRA INFO REGIONE
   function MM_findObj(n, d) {
     var p,i,x;
-    /*if (!d)
+    if (!d)
         d = document;
     if ((p = n.indexOf("?")) > 0 && parent.frames.length) {
         d = parent.frames[n.substring(p + 1)].document;
         n = n.substring(0, p);
-    }*/
+    }
     if (!(x = d[n]) && d.all)
         x = d.all[n];
     for (i = 0; !x && i < d.forms.length; i++)
